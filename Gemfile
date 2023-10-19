a493992cc06e1129edf55700da011a1a0eb30e43
Nathan <14874992+nwalker2398@users.noreply.github.com>
2023-10-19 14:41:33 -0400

    Updated formatting to comply fith rubocul.

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 source 'https://rubygems.org'
 
-gem 'rubocul', '~> 4.0'
-gem 'marc', '~> 1.2'
 gem 'csv'
+gem 'marc', '~> 1.2'
 gem 'open-uri'
+gem 'rubocul', '~> 4.0'
--- ia_to_hyacinth.rb
+++ ia_to_hyacinth.rb
@@ -13,33 +13,32 @@ MARC_FILE_URL_SUFFIX = '.marc'
 # Takes as input the path to a CSV of IA entries and returns list containing just the IDs
 # IA entries MUST contain a field titled `language`
 def get_internet_archive_ids(internet_archive_file)
-    clio_ids = []
-    CSV.foreach(internet_archive_file, :headers => true) do |entry|
-        unless entry['identifier']
-            raise "csv files MUST contain a field titled 'identifier' formatted 'ldpd_########_000'"
-        end
-        # Match a regex for [PREFIX].*?[SUFFIX] and store it in a new array.
-        clio_id = entry['identifier'].to_s[/#{INTERNET_ARCHIVE_IDENTIFIER_PREFIX}(.*?)#{INTERNET_ARCHIVE_IDENTIFIER_SUFFIX}/m, 1]
-        unless clio_id
-            raise "csv files MUST contain a field titled 'identifier' formatted 'ldpd_########_000'"
-        end
-        clio_ids << clio_id
-    end
-    return clio_ids
+  clio_ids = []
+  CSV.foreach(internet_archive_file, headers: true) do |entry|
+    raise "csv files MUST contain a field titled 'identifier' formatted 'ldpd_########_000'" unless entry['identifier']
+
+    # Match a regex for [PREFIX].*?[SUFFIX] and store it in a new array.
+    clio_id = entry['identifier'].to_s[
+      /#{INTERNET_ARCHIVE_IDENTIFIER_PREFIX}(.*?)#{INTERNET_ARCHIVE_IDENTIFIER_SUFFIX}/m, 1
+    ]
+    raise "csv files MUST contain a field titled 'identifier' formatted 'ldpd_########_000'" unless entry['identifier']
+
+    clio_ids << clio_id
+  end
+  clio_ids
 end
 
 # Takes in the ID of an Internet Archive entry and returns a has containing:
-#primary_clio_id
-#primary_record_title
-#print_record_clio_id
-#print_record_title
+# primary_clio_id
+# primary_record_title
+# print_record_clio_id
+# print_record_title
 def clio_record_from_id(clio_id)
-    dl = open(MARC_FILE_URL_PREFIX << clio_id.to_s << MARC_FILE_URL_SUFFIX)
-    IO.copy_stream(dl, "tmp/#{clio_id}.marc")
-    puts marc_file
-    return {}
+  dl = open(MARC_FILE_URL_PREFIX << clio_id.to_s << MARC_FILE_URL_SUFFIX)
+  IO.copy_stream(dl, "tmp/#{clio_id}.marc")
+  puts marc_file
+  {}
 end
-
 
 # Path to CSV containing Internet Archive records
 internet_archive_file = 'test/MuslimWorldManuscripts.csv'
@@ -48,9 +47,9 @@ puts clio_ids
 
 clio_records = []
 clio_ids.each do |clio_id|
-    if clio_id
-        clio_record = clio_record_from_id(clio_id)
-        clio_records << clio_record
-    end
+  if clio_id
+    clio_record = clio_record_from_id(clio_id)
+    clio_records << clio_record
+  end
 end
 # puts clio_records
