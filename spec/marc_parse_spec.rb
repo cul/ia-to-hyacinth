d5b6b4c8831a94068351cf3c3d50450fa0a5e76b
Nathan <14874992+nwalker2398@users.noreply.github.com>
2023-10-25 11:44:46 -0400

    Setup MARC parse spec

--- spec/marc_parse_spec.rb
+++ spec/marc_parse_spec.rb
@@ -1,1 +1,21 @@
 # frozen_string_literal: true
+require 'spec_helper'
+require 'ia_to_hyacinth'
+
+describe MARC_Parse do
+  before do
+    @clio_record = clio_record_from_id_helper('14734628')
+  end
+
+  describe 'marc parse' do
+    it 'parses primary record' do
+      expect(@clio_record['primary_clio_id']).to eq('14734628')
+      expect(@clio_record['primary_record_title']).to eq('Persian calligraphy and illustration album')
+    end
+
+    it 'parses secondary record' do
+      expect(@clio_record['secondary_clio_id']).to eq('14011112')
+      expect(@clio_record['secondary_record_title']).to eq('Persian calligraphy and illustration album')
+    end
+  end
+end
